1778be8435f69d0c0b317c5d4671ccea4ce17273
Ivan <offlane@tut.by>
2022-11-03 01:11:07 +0300

    Получение данных

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,35 @@ import { Container } from 'components/Container';
 import { Header } from 'components/Header';
 import { Search } from 'components/Search';
 import { UserCard } from 'components/UserCard';
-import React from 'react';
-
+import React, { useState } from 'react';
+import { GithubError, GithubUser, LocalGithubUser } from 'types';
+import { extractLocalUser } from 'utils/extract-local-user';
+import { isGithubUser } from 'utils/typeguards';
 import { defaultUser } from './mock/index';
 
+const BASE_URL = 'https://api.github.com/users/';
+
 function App() {
+  const [user, setUser] = useState<LocalGithubUser | null>(defaultUser);
+
+  const fetchUser = async (username: string) => {
+    const url = BASE_URL + username;
+
+    const res = await fetch(url);
+    const user = (await res.json()) as GithubUser | GithubError;
+
+    if (isGithubUser(user)) {
+      setUser(extractLocalUser(user));
+    } else {
+      setUser(null);
+    }
+  };
+
   return (
     <Container>
       <Header />
-      <Search hasError onSubmit={() => {}} />
-      <UserCard {...defaultUser} />
+      <Search hasError={!user} onSubmit={fetchUser} />
+      {user && <UserCard {...user} />}
     </Container>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import styles from './Header.module.scss';
 export const Header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.logo}>GH User Search</div>
+      <div className={styles.logo}>GitHub User Search</div>
       <ThemeSwitcher />
     </div>
   );

91485ccbbfbfc76df6d6c036294e6a0d61aaa8f9
Ivan <offlane@tut.by>
2022-11-02 20:50:01 +0300

    Компонент поиска

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Container } from 'components/Container';
 import { Header } from 'components/Header';
+import { Search } from 'components/Search';
 import React from 'react';
 
 function App() {
   return (
     <Container>
       <Header />
+      <Search hasError onSubmit={() => {}} />
     </Container>
   );
 }

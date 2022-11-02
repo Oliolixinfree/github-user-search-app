dd263307f844671fd108fc34b4f1baf7b8b43398
Ivan <offlane@tut.by>
2022-11-02 19:16:12 +0300

    Fix TemplateName.tsx

--- templates/default/TemplateName.tsx
+++ templates/default/TemplateName.tsx
@@ -4,7 +4,5 @@ import styles from './TemplateName.module.scss';
 interface TemplateNameProps {}
 
 export const TemplateName = ({}: TemplateNameProps) => (
-  <div className={styles.TemplateName} data-testid="TemplateName">
-    TemplateName Component
-  </div>
+  <div className={styles.templateName}>TemplateName Component</div>
 );

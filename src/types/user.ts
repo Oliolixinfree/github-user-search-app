c18429e9a96c867fdcfe912c72b1715cea4b7b19
Ivan <offlane@tut.by>
2022-11-02 22:18:17 +0300

    Fix type GithubUser

--- src/types/user.ts
+++ src/types/user.ts
@@ -19,11 +19,11 @@ export type GithubUser = {
   id: number;
   avatar_url: string;
   name: string;
-  company: string;
+  company: string | null;
   blog: string;
-  location: string;
+  location: string | null;
   bio: string;
-  twitter_username: string;
+  twitter_username: string | null;
   public_repos: number;
   followers: number;
   following: number;

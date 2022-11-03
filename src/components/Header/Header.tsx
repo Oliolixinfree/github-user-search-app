25f0b1281b742e26a7fe6a07a13970a0d143d03c
Ivan <offlane@tut.by>
2022-11-03 21:59:51 +0300

    Изменены стили, добавлено svg лого

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,18 @@
 import { ThemeSwitcher } from 'components/ThemeSwitcher';
 import React from 'react';
 import styles from './Header.module.scss';
+import { ReactComponent as Logo } from '../../assets/icon-github-logo.svg';
 
 export const Header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.logo}>GitHub User Search</div>
+      <div className={styles.logoBlock}>
+        <Logo />
+        <div className={styles.logo}>
+          GitHub
+          <br /> User Search
+        </div>
+      </div>
       <ThemeSwitcher />
     </div>
   );
